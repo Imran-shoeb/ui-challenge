eb15b8419e1950ac0805b1d0e9f83021206bd942
Imranul Islam <shoeb1219@gmail.com>
2022-02-04 16:22:50 +0900

    added all the requirements and tests

--- src/app/products/products.component.spec.ts
+++ src/app/products/products.component.spec.ts
@@ -1,16 +1,23 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { SeedService } from '../services/seed.service';
 
 import { ProductsComponent } from './products.component';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let seedService: SeedService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+      declarations: [ ProductsComponent ],
+      providers: [
+        {provide: SeedService}
+      ]    
     })
     .compileComponents();
+    seedService = TestBed.inject(SeedService);
   });
 
   beforeEach(() => {
@@ -22,4 +29,29 @@ describe('ProductsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title in a h1 tag', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain('Popular Products');
+  });
+
+  it('should call seedService once when document loads', () => {
+    expect(seedService.getAllProducts.call.length)
+      .withContext('getAllProducts method was called once')
+      .toBe(1);
+  });
+
+  it('getAllProducts() should return valid data', () => {
+    let productList = seedService.getAllProducts();
+    expect(typeof productList).toBe('object');
+    expect(typeof productList[0].id).toBe('number');
+  });
+
+  it('vote button clicked and increment function called', fakeAsync(() => {
+    spyOn(component, 'increment');
+    let button = fixture.debugElement.nativeElement.querySelector('.vote');
+    button.click();
+    tick();
+    expect(component.increment).toHaveBeenCalled();
+  }));
 });
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -4,17 +4,30 @@ import { SeedService } from '../services/seed.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-
   productList: any[] = [];
-  constructor(private seedService: SeedService) { }
+  constructor(private seedService: SeedService) {}
 
   ngOnInit(): void {
-
-    this.productList = this.seedService.getAllProducts();
-    console.log(this.productList);
+    this.sortingItemsDescending();
   }
 
+  sortingItemsDescending() {
+    this.productList = this.seedService.getAllProducts();
+    this.productList.sort(function (a, b) {
+      return b.votes - a.votes;
+    });
+  }
+
+  increment(id: any) {
+    for (var i in this.productList) {
+      if (id === this.productList[i].id) {
+        this.productList[i].votes++;
+        this.sortingItemsDescending();
+        break;
+      }
+    }
+  }
 }
